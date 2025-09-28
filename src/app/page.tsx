c9f68672a4d0875ed488f8d2801a78a91145174f
RockN <gold_100@bk.ru>
2025-09-28 22:40:49 +0300

    fix fetch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,22 @@
 import { adapterSkillsData } from "@/utils/adapterSkillsData";
+import mockJson from "../mocks/data_ru.json";
 import IndexPage from "./IndexPage";
 
 const getData = async () => {
   const env = process.env;
-  const response = await fetch(env.NEXT_PUBLIC_SKILLS_DATA as string);
-  if (!response.ok) {
-    throw new Error("Error fetch");
+
+  try {
+    const response = await fetch(env.NEXT_PUBLIC_SKILLS_DATA as string);
+    if (!response.ok) {
+      throw new Error("Error fetch");
+    }
+
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log("error", error);
+    return mockJson;
   }
-
-  const data = await response.json();
-
-  return data;
 };
 
 export default async function Home() {
